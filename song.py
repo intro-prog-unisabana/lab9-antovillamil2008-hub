d88a18212cf45b4324a3b16f4c0eb5c9c73f71e9
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-05-01 10:29:49 -0500

    1

--- song.py
+++ song.py
@@ -1,12 +1,17 @@
 class Song:
 
     def __init__(self, name, artist, length):
-        self.name=str(name)
-        self.artist=str(artist)
-        self.length=float(length)
+        self.name = str(name)
+        self.artist = str(artist)
+        self.length = float(length)
+    
+    def __str__(self):
+        return f"'{self.name}' by {self.artist} {self.length}"
     
     def get_length_in_seconds(self):
         return self.length*60
-        
+
+
 my_song = Song("tv off", "Kendrick Lamar", 3.7)
 print(my_song.get_length_in_seconds())
+print(my_song)
